b55b68f9075c00d8cca1155ac04a95182ffc1437
AlvaroDosio <dosioalvaromartin@gmail.com>
2021-08-05 19:06:12 -0300

    added patch to couponRoutes

--- typeorm/src/routes/couponRoutes.ts
+++ typeorm/src/routes/couponRoutes.ts
@@ -1,9 +1,10 @@
 import {Router} from "express"
 const router = Router()
-import {getCoupon, postCoupon} from "../controllers/coupon.controller"
+import {getCoupon,postCoupon,patchCoupon} from "../controllers/coupon.controller"
 
 router.get("/coupons",getCoupon )
 router.post("/coupons", postCoupon)
+router.patch("/coupons", patchCoupon)
     
 
 export default router
