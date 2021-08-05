64f31267106ae8b1c336bbabfe608c831a1d188d
AlvaroDosio <dosioalvaromartin@gmail.com>
2021-08-05 18:57:08 -0300

    deleted .js files, to work with .ts files only

--- typeorm/src/routes/couponRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1.Router();
-var coupon_controller_1 = require("../controllers/coupon.controller");
-router.get("/coupons", coupon_controller_1.getCoupon);
-router.post("/coupons", coupon_controller_1.postCoupon);
-exports["default"] = router;
--- typeorm/src/routes/storeRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1.Router();
-var store_controller_1 = require("../controllers/store.controller");
-router.get("/stores", store_controller_1.getStores);
-exports["default"] = router;
