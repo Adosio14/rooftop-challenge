f2b104365d7214d3c634676351248eaf9f57a2d7
AlvaroDosio <dosioalvaromartin@gmail.com>
2021-08-06 18:04:09 -0300

    created getStores

--- typeorm/src/controllers/store.controller.ts
+++ typeorm/src/controllers/store.controller.ts
@@ -1,20 +1,42 @@
 import { Response, Request } from "express";
 import "dotenv/config";
-import { getRepository } from "typeorm";
+import { FindOperator, getRepository, Like } from "typeorm";
 import Store from "../entity/store"
+import { any } from "joi";
 
 
-export const getStores = async (req: Request, res:Response)=>{
-    getRepository(Store)
-    .find()
-    .then((data)=>{
-        res.status(200).json({
-            data: data,
-        });
-    }).catch((err)=>{
-        res.status(400).json({
-            message: err,
-        });
-    })
+interface storeQueryParams{
+    name?: string,
+    page?: string;
+}
+interface nameInterface{
+    where?: any,
+    take: 10,
+    skip : number
+}
+export const getStores = async (req: Request<{},{},{},storeQueryParams>, res:Response)=>{
+    const name: string = (<string>req.query.name)
+    const page: number = parseInt(<string>req.query.page)
+    const repository = await getRepository(Store)
+    console.log(typeof page)
+    const options: nameInterface={
+        where: {},
+        take : 10,
+        skip : 0,
+        
+    }
+    if(page){
+        options.skip = (page - 1) * options.take
+    }
+    if(name){
+        options.where.name = Like("%"+name+"%")
+    }
+    console.log(options)
+    try{    
+        const data = await repository.find(options)
+        res.status(200).send(data)
+    }catch{
+        res.status(404).send({message: "There is no stores!"})
+    }
 }
 
