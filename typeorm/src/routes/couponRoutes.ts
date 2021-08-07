a3b7ad31a19680092f24621921d557e1e8a9b4f1
AlvaroDosio <dosioalvaromartin@gmail.com>
2021-08-07 14:18:27 -0300

    added delete to coupons route

--- typeorm/src/routes/couponRoutes.ts
+++ typeorm/src/routes/couponRoutes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express"
 const router = Router()
-import {getCoupon,postCoupon,patchCoupon} from "../controllers/coupon.controller"
+import {getCoupon,postCoupon,patchCoupon, deleteCoupon} from "../controllers/coupon.controller"
 
 router.get("/coupons",getCoupon )
 router.post("/coupons", postCoupon)
 router.patch("/coupons", patchCoupon)
+router.delete("/coupons", deleteCoupon)
     
 
 export default router
